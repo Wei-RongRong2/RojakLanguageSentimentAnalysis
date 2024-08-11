c64aaef66fb033c7aaf571287b35a8da3551763f
Wrrrrr <102149027+Wei-RongRong2@users.noreply.github.com>
2024-08-11 15:12:29 +0800

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import tensorflow as tf
 import pickle
@@ -45,4 +46,5 @@ def index():
     return render_template('index.html', prediction=prediction, image_url=image_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT if available, otherwise default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
